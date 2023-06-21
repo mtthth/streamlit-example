526645dbaa6fdb5d64c322b7a58c0eeac842dd59
cidrolin <matthieu.thomas@gmail.com>
2023-06-21 13:31:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('Météo BW')
 
 
 def load_temperature():
-    temperature = pd.read_csv(r"X:\bw\environnement\2023_06_températures.txt", sep='\t', names=['MJD','T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9'])
+    temperature = pd.read_csv(r".\X:\bw\environnement\2023_06_températures.txt", sep='\t', names=['MJD','T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9'])
     #tilt =  pd.read_csv(r"X:\bw\environnement\2023_06_leica_nivel_20.txt", sep='\t', names=['MJD','X', 'Y', 'T', 'void'])
     return temperature
 
